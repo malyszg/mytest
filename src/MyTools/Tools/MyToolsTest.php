c6a988f02219d1af090a27f51abe4b6c884e8c38
gtesmer <grzegorz.tesmer@morizon.pl>
2021-08-03 19:35:48 +0200

    test

---
+++ src/MyTools/Tools/MyToolsTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gtesmer
+ * Date: 2021-08-03
+ * Time: 19:34
+ */
+
+namespace MyTools\Tools;
+
+
+class MyToolsTest
+{
+    public function getTest()
+    {
+        return 'this is test';
+    }
+}
